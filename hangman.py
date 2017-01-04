50f7075c165452149e13ff307ab516b651ed8312
darkwoolf10 <woolftreuser@yandex.ru>
2017-01-04 03:10:10 +0200

    update errors

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def begin():
     print(" =====||")
     print(" |    ||")
@@ -19,7 +20,6 @@ def oneError():
     print("      ||")
     print("========")
 
-
 def twoError():
     print(" =====||")
     print(" |    ||")
@@ -29,7 +29,6 @@ def twoError():
     print("      ||")
     print("      ||")
     print("========")
-
 
 def threeError():
     print(" =====||")
@@ -41,7 +40,6 @@ def threeError():
     print("      ||")
     print("========")
 
-
 def fourError():
     print(" =====||")
     print(" |    ||")
@@ -51,7 +49,6 @@ def fourError():
     print("      ||")
     print("      ||")
     print("========")
-
 
 def fiveError():
     print(" =====||")
@@ -63,7 +60,6 @@ def fiveError():
     print("      ||")
     print("========")
 
-
 def sixError():
     print(" =====||")
     print(" |    ||")
@@ -74,19 +70,47 @@ def sixError():
     print("      ||")
     print("========")
 
-
 def end():
     print("|||||||  || \    ||    ||+++")
     print("||       ||\ \   ||    ||   ++")
     print("|||||||  || \ \  ||    ||   ++")
     print("||       ||  \ \ ||    ||   ++")
     print("|||||||  ||   \ \||    ||+++")
-puzzle = "HANGMAN"
+puzzle = "HANGMAN"#загаданое слово
 begin()
-errors = len(puzzle)
+length = len(puzzle)
+errors = 0
+output = ""
 i = 0
 char = ""
-while(i < errors):
-    char += input()
+#цикл ввода символов
+while True:
+    chars = 0
+    char = input()
+    for key in puzzle:
+        if key == char:
+            key = char
+            print(key)
+            output += key
+            chars += 1
+
+        #количество ошибок
+        if errors == 1:
+            oneError()
+        elif errors == 2:
+            twoError()
+        elif errors == 3:
+            threeError()
+        elif errors == 4:
+            fourError()
+        elif errors == 5:
+            fiveError()
+        elif errors == 6:
+            sixError()
+    if chars == 0:
+         errors += 1
+    if errors == 7:
+        end()
+        break
     i += 1
-    print(char)
+print(output)
