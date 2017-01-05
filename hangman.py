58bfe6b346ebfca6c1af8a21fae633e883e12eb9
darkwoolf10 <woolftreuser@yandex.ru>
2017-01-06 01:13:42 +0200

    transfer pictures

--- hangman.py
+++ hangman.py
@@ -1,116 +1,59 @@
-#!/usr/bin/python3
-def begin():
-    print(" =====||")
-    print(" |    ||")
-    print("      ||")
-    print("      ||")
-    print("      ||")
-    print("      ||")
-    print("      ||")
-    print("========")
+# !/usr/bin/python3
+# Короче тебе надо если char in puzzle искать re.find_all по чару в
+# puzzle. После, по найденым индексам (в цикле, даже если
+# буква одна) менять по индексу буквы в secret и вывести secret
 
+from pictures import *
 
-def oneError():
-    print(" =====||")
-    print(" |    ||")
-    print(" 0    ||")
-    print("      ||")
-    print("      ||")
-    print("      ||")
-    print("      ||")
-    print("========")
-
-def twoError():
-    print(" =====||")
-    print(" |    ||")
-    print(" 0    ||")
-    print(" |    ||")
-    print("      ||")
-    print("      ||")
-    print("      ||")
-    print("========")
-
-def threeError():
-    print(" =====||")
-    print(" |    ||")
-    print(" 0    ||")
-    print("/|    ||")
-    print("      ||")
-    print("      ||")
-    print("      ||")
-    print("========")
-
-def fourError():
-    print(" =====||")
-    print(" |    ||")
-    print(" 0    ||")
-    print("/|\   ||")
-    print("      ||")
-    print("      ||")
-    print("      ||")
-    print("========")
-
-def fiveError():
-    print(" =====||")
-    print(" |    ||")
-    print(" 0    ||")
-    print("/|\   ||")
-    print(" |    ||")
-    print("/     ||")
-    print("      ||")
-    print("========")
-
-def sixError():
-    print(" =====||")
-    print(" |    ||")
-    print(" 0    ||")
-    print("/|\   ||")
-    print(" |    ||")
-    print("/ \   ||")
-    print("      ||")
-    print("========")
-
-def end():
-    print("|||||||  || \    ||    ||+++")
-    print("||       ||\ \   ||    ||   ++")
-    print("|||||||  || \ \  ||    ||   ++")
-    print("||       ||  \ \ ||    ||   ++")
-    print("|||||||  ||   \ \||    ||+++")
-puzzle = "HANGMAN"#загаданое слово
+puzzle = "HANGMAN"# загаданое слово
 begin()
 length = len(puzzle)
 errors = 0
 output = ""
 i = 0
 char = ""
+secret = "_" * length
+print(secret)
 #цикл ввода символов
 while True:
     chars = 0
     char = input()
     for key in puzzle:
         if key == char:
-            key = char
+            secret[] = key
             print(key)
             output += key
-            chars += 1
+            chars += 1  # Если не найдт совпадений, то в конце цикла выполнеться условие
 
-        #количество ошибок
-        if errors == 1:
-            oneError()
-        elif errors == 2:
-            twoError()
-        elif errors == 3:
-            threeError()
-        elif errors == 4:
-            fourError()
-        elif errors == 5:
-            fiveError()
-        elif errors == 6:
-            sixError()
+    # количество ошибок
+    if errors == 1:
+        oneError()
+
+    elif errors == 2:
+        twoError()
+    
+    elif errors == 3:
+        threeError()
+    
+    elif errors == 4:
+        fourError()
+    
+    elif errors == 5:
+        fiveError()
+    
+    elif errors == 6:
+        sixError()
+
+
     if chars == 0:
          errors += 1
+    
     if errors == 7:
         end()
         break
+    
+
     i += 1
+
+
 print(output)
---
+++ pictures.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+def begin():
+    print(" =====||")
+    print(" |    ||")
+    print("      ||")
+    print("      ||")
+    print("      ||")
+    print("      ||")
+    print("      ||")
+    print("========")
+
+
+def oneError():
+    print(" =====||")
+    print(" |    ||")
+    print(" 0    ||")
+    print("      ||")
+    print("      ||")
+    print("      ||")
+    print("      ||")
+    print("========")
+
+
+def twoError():
+    print(" =====||")
+    print(" |    ||")
+    print(" 0    ||")
+    print(" |    ||")
+    print("      ||")
+    print("      ||")
+    print("      ||")
+    print("========")
+
+
+def threeError():
+    print(" =====||")
+    print(" |    ||")
+    print(" 0    ||")
+    print("/|    ||")
+    print("      ||")
+    print("      ||")
+    print("      ||")
+    print("========")
+
+
+def fourError():
+    print(" =====||")
+    print(" |    ||")
+    print(" 0    ||")
+    print("/|\   ||")
+    print("      ||")
+    print("      ||")
+    print("      ||")
+    print("========")
+
+
+def fiveError():
+    print(" =====||")
+    print(" |    ||")
+    print(" 0    ||")
+    print("/|\   ||")
+    print(" |    ||")
+    print("/     ||")
+    print("      ||")
+    print("========")
+
+
+def sixError():
+    print(" =====||")
+    print(" |    ||")
+    print(" 0    ||")
+    print("/|\   ||")
+    print(" |    ||")
+    print("/ \   ||")
+    print("      ||")
+    print("========")
+
+
+def end():
+    print("|||||||  || \    ||    ||+++")
+    print("||       ||\ \   ||    ||   ++")
+    print("|||||||  || \ \  ||    ||   ++")
+    print("||       ||  \ \ ||    ||   ++")
+    print("|||||||  ||   \ \||    ||+++")
